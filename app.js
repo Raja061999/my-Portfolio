cc306a63ac3b9a87d42413ca34bb9c59f0fbcabe
raja@raja <sraja061999@gmail.com>
2023-05-30 07:06:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+    /*document.getElementById('menu').addEventListener('click',function(){
+        document.getElementById('header').setAttribute('class','toggle');
+        document.getElementsByTagName('body').setAttribute('class','body1');
+    })
+   
